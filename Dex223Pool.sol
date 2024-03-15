dd15a3ac803303dbea564332b92b70cc8a2fa748
Dexaran <dexaran820@gmail.com>
2024-03-15 23:06:19 +0400

    Test

--- Dex223Pool.sol
+++ Dex223Pool.sol
@@ -857,32 +857,36 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
             if (amount1 < 0) TransferHelper.safeTransfer(token1, recipient, uint256(-amount1));
 
             // ERC-223 depositing logic
+            /*
             if (erc223deposit[msg.sender][token0] >= uint256(amount0))
             {
                 erc223deposit[msg.sender][token0] -= uint256(amount0);
             }
+            */
             // ERC-20 depositing logic
-            else 
-            {
+            //else 
+            //{
                 uint256 balance0Before = balance0();
                 IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(amount0, amount1, data);
                 require(balance0Before.add(uint256(amount0)) <= balance0(), 'IIA');
-            }
+            //}
         } else {
             if (amount0 < 0) TransferHelper.safeTransfer(token0, recipient, uint256(-amount0));
 
             // ERC-223 depositing logic
+            /*
             if (erc223deposit[msg.sender][token1] >= uint256(amount1))
             {
                 erc223deposit[msg.sender][token1] -= uint256(amount1);
             }
+            */
             // ERC-20 depositing logic
-            else 
-            {
+            //else 
+            //{
                 uint256 balance1Before = balance1();
                 IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(amount0, amount1, data);
                 require(balance1Before.add(uint256(amount1)) <= balance1(), 'IIA');
-            }
+            //}
         }
 
         emit Swap(msg.sender, recipient, amount0, amount1, state.sqrtPriceX96, state.liquidity, state.tick);
