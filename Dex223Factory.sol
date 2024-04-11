6de8d4726f8749a5e7c469183b8b3bd4fd1910c5
Dexaran <dexaran820@gmail.com>
2024-04-12 00:53:35 +0400

    Update Dex223Factory.sol

--- Dex223Factory.sol
+++ Dex223Factory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.7.6;
 
-import './interfaces/IUniswapV3Factory.sol';
+import './interfaces/IDex223Factory.sol';
 
 import './interfaces/ITokenConverter.sol';
 import './interfaces/ITokenStandardIntrospection.sol';
