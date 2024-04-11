a3e6db045b091156b84b75b0aafff344cfbc0b27
Dexaran <dexaran820@gmail.com>
2024-04-11 21:12:40 +0400

    Update IUniswapV3PoolImmutables.sol

--- interfaces/pool/IUniswapV3PoolImmutables.sol
+++ interfaces/pool/IUniswapV3PoolImmutables.sol
@@ -8,14 +8,6 @@ interface IUniswapV3PoolImmutables {
     /// @return The contract address
     function factory() external view returns (address);
 
-    /// @notice The first of the two tokens of the pool, sorted by address
-    /// @return The token contract address
-    function token0() external view returns (address);
-
-    /// @notice The second of the two tokens of the pool, sorted by address
-    /// @return The token contract address
-    function token1() external view returns (address);
-
     /// @notice The pool's fee in hundredths of a bip, i.e. 1e-6
     /// @return The fee
     function fee() external view returns (uint24);
