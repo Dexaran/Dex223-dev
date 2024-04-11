84a20ecda24f08cb8551d5ff4ffa014272d8f358
Dexaran <dexaran820@gmail.com>
2024-04-11 22:05:10 +0400

    Update Dex223PoolDeployer.sol

--- Dex223PoolDeployer.sol
+++ Dex223PoolDeployer.sol
@@ -1,22 +1,25 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.7.6;
 
-import './interfaces/IUniswapV3PoolDeployer.sol';
+import './interfaces/IDex223PoolDeployer.sol';
 
 import './Dex223Pool.sol';
 
-contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
+contract UniswapV3PoolDeployer is IDex223PoolDeployer {
     struct Parameters {
         address factory;
-        address token0;
-        address token1;
+        address token0_erc20;
+        address token1_erc20;
+        address token0_erc223;
+        address token1_erc223;
         uint24 fee;
         int24 tickSpacing;
     }
 
-    /// @inheritdoc IUniswapV3PoolDeployer
+    /// @inheritdoc IDex223PoolDeployer
     Parameters public override parameters;
 
+/*
     /// @dev Deploys a pool with the given parameters by transiently setting the parameters storage slot and then
     /// clearing it after deploying the pool.
     /// @param factory The contract address of the Uniswap V3 factory
@@ -24,15 +27,18 @@ contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
     /// @param token1 The second token of the pool by address sort order
     /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
     /// @param tickSpacing The spacing between usable ticks
+*/
     function deploy(
         address factory,
-        address token0,
-        address token1,
+        address token0_erc20,
+        address token1_erc20,
+        address token0_erc223,
+        address token1_erc223,
         uint24 fee,
         int24 tickSpacing
     ) internal returns (address pool) {
-        parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
-        pool = address(new Dex223Pool{salt: keccak256(abi.encode(token0, token1, fee))}());
+        parameters = Parameters({factory: factory, token0_erc20: token0_erc20, token1_erc20: token1_erc20, token0_erc223: token0_erc223, token1_erc223: token1_erc223, fee: fee, tickSpacing: tickSpacing});
+        pool = address(new Dex223Pool{salt: keccak256(abi.encode(token0_erc20, token1_erc20, fee))}());
         delete parameters;
     }
 }
