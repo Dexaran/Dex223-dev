64e92046d4914c438c42018370c02b2b5418b5ea
Dexaran <dexaran820@gmail.com>
2024-04-12 00:51:50 +0400

    Update IUniswapV3Factory.sol

--- interfaces/IUniswapV3Factory.sol
+++ interfaces/IUniswapV3Factory.sol
@@ -3,21 +3,25 @@ pragma solidity >=0.5.0;
 
 /// @title The interface for the Uniswap V3 Factory
 /// @notice The Uniswap V3 Factory facilitates creation of Uniswap V3 pools and control over the protocol fees
-interface IUniswapV3Factory {
+interface IDex223Factory {
     /// @notice Emitted when the owner of the factory is changed
     /// @param oldOwner The owner before the owner was changed
     /// @param newOwner The owner after the owner was changed
     event OwnerChanged(address indexed oldOwner, address indexed newOwner);
 
+/*
     /// @notice Emitted when a pool is created
     /// @param token0 The first token of the pool by address sort order
     /// @param token1 The second token of the pool by address sort order
     /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
     /// @param tickSpacing The minimum number of ticks between initialized ticks
     /// @param pool The address of the created pool
+*/
     event PoolCreated(
-        address indexed token0,
-        address indexed token1,
+        address indexed token0_erc20,
+        address indexed token1_erc20,
+        address token0_erc223,
+        address token1_erc223,
         uint24 indexed fee,
         int24 tickSpacing,
         address pool
